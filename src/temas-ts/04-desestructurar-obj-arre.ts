83b88e300eba6fcc43f3d2a1ade17a3fffc36c02
Alan Jeremy Lara Hurtado <alanhurtado1313@gmail.com>
2024-09-12 20:39:58 -0600

    Segundo commit del martes

--- src/temas-ts/04-desestructurar-obj-arre.ts
+++ src/temas-ts/04-desestructurar-obj-arre.ts
@@ -1,1 +1,50 @@
 
+interface Reproductor{
+    volumen:number;
+    segundo:number;
+    cancion:string;
+    detalles:Detalles;
+}
+
+interface Detalles{
+    autor:string;
+    year:number;
+}
+const reproductor:Reproductor={
+    volumen:50,
+    segundo:30,
+    cancion:"La vida es un sueño",
+    detalles:{
+        autor:"Luis Miguel",
+        year:2002
+    }
+
+}
+/*
+console.log(`El volumen actual es: ${reproductor.volumen} `)
+console.log(`Los segundos son: ${reproductor.segundo} `)
+console.log(`La canción es: ${reproductor.cancion} `) 
+console.log(`El cantante es: ${reproductor.detalles.autor} `)
+console.log(`El año es: ${reproductor.detalles.year} `)*/
+
+const{volumen, segundo, cancion, detalles}=reproductor;
+const{autor,year}=detalles;
+
+console.log(`El volumen actual es: ${volumen} `)
+console.log(`Los segundos son: ${segundo} `)
+console.log(`La canción es: ${cancion} `) 
+console.log(`El cantante es: ${autor} `)
+console.log(`El año es: ${year} `)
+
+/*
+const dbz:string[]=['Goku','Majinbu', 'Babidy', 'Freezer']
+console.log(Personaje 1: ${dbz[0]})
+console.log(Personaje 2: ${dbz[1]})
+console.log(Personaje 3: ${dbz[2]})
+console.log(Personaje 4: ${dbz[3]})
+
+const[a,,b,c]=dbz;
+console.log(Personaje 1: ${a})
+console.log(Personaje 2: ${b})
+console.log(Personaje 3: ${c})
+*/
---
+++ src/temas-ts/07-clases.ts
@@ -0,0 +1,16 @@
+
+class Persona{
+    nombre:string;
+    edad:number;
+    constructor(nombre:string, edad1:number,){
+        this.nombre = nombre;
+        this.edad = edad1;
+
+    }
+    imprimir():void{
+        console.log(`Hola mi nombre es ${this.nombre} y
+            tengo ${this.edad}`);
+    }
+}
+let persona1 = new Persona('Juan', 30);
+console.log(persona1)
